c8c81a69f9bcf4cc2a4663f2ec49249c955edff4
elisabeth-parker <elabeth@live.unc.edu>
2018-03-07 09:33:50 -0500

    adding content from monday 3/5

---
+++ jSchool/views.py
@@ -0,0 +1,11 @@
+#jSchool/views.py
+
+from django.shortcuts import render
+from jSchool.models import Course, Student
+
+def home(request):
+    context = {
+        'student_count': Student.objects.count(),
+        'course_count': Course.objects.count(),
+    }
+    return render(request, "home.html", context)
